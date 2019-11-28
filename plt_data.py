48ee9959247939237abdf74aa5db8baa8dc35999
Adreeja <bardhan.adreeja1@gmail.com>
2019-11-28 10:25:49 +0530

    Update plt_data.py

--- plt_data.py
+++ plt_data.py
@@ -1,10 +1,10 @@
 import matplotlib
 import matplotlib.pyplot as plt
-def simple(*sers):
-	for i in sers:
+def simple(legend,*sers):
+	for v,i in enumerate(sers):
 		#print i
 		#print i.index
 		i=i.sort_index()
-		plt.plot(i.index,i,data=i)
+		plt.plot(i.index,i,data=i,label=legend[v])
 	plt.show()
 	
