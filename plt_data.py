5bdde6bb24540e3ae775d423260dce78e9b47100
Adreeja <bardhan.adreeja1@gmail.com>
2019-12-28 20:33:43 +0530

    Update plt_data.py

--- plt_data.py
+++ plt_data.py
@@ -8,3 +8,21 @@ def simple(legend,*sers):
 		plt.plot(i.index,i,data=i,label=legend[v])
 	plt.show()
 	
+
+def candle(seq):
+	ax1=plt.gca()
+	candlestick_ohlc(ax1,seq)
+	ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+	plt.show()
+
+def roll(ser,*periods):
+	roll1=ser
+	#rollres
+	for v,i in enumerate(periods):
+		rollres=roll1.rolling(i,center=True).mean()
+		plt.plot(rollres.index,rollres,data=rollres,label=str(i))
+		#print i
+	plt.plot(ser.index,ser,data=ser,label="original")
+	plt.legend()
+	plt.show()
+		
